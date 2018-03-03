3260d3181c7e6b50f11498236c20fbed5eb99e98
lazar <lazar@spartans.tech>
2018-03-03 03:22:27 +0100

    :rocket:

--- routes.js
+++ routes.js
@@ -1,9 +1,86 @@
 require('dotenv').config();
 const conversation = require('./conversation');
+const request = require('request');
+const uuidv4 = require('uuid/v4');
 
+
+
+const pythonURL = '';
+const javaURL = '';
 
 module.exports = (app, db) => {
 
+    // 1) When someone is near android send me ID and station id
+    app.post('/near', async (res, req) => {
+        try {
+            const {
+                id,
+                stationId
+            } = req.body;
+
+            console.log(`ID: ${id}`);
+            console.log(`Station id: ${stationId}`);
+
+            const json = {
+                id,
+                stationId
+            }
+
+            // 2) Redirect this informations to python
+            httpRequest.post(pythonURL, {
+                json
+            }, (err, httpResponse, body) => {
+                console.log(httpResponse.statusCode);
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Request successfully send');
+                }
+            });
+
+            res.sendStatus(200);
+
+        } catch (error) {
+            console.log('Near error:', error);
+        }
+    });
+
+    // 3) Endpoint where I get offer and all data from DB
+    app.post('/offer', async (req, res) => {
+        const {
+            data
+        } = req.body;
+
+        const token = uuidv4(); // ⇨ '416ac246-e7ac-49ff-93b4-f7e94d997e6b'
+        console.log('Genereted UUID: ', token);
+        
+        const json = {
+            data,
+            token,
+            date: Date.now()
+        };
+
+        // 4) Send to java this data
+        httpRequest.post(javaURL, {
+            json
+        }, (err, httpResponse, body) => {
+            console.log(httpResponse.statusCode);
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Request successfully send');
+            }
+        });
+
+    });
+
+
+
+
+
+
+
+    //////////////// SEKTA
     app.get('/', (req, res) => {
         console.log('AJDE PRORADI')
         res.send('Jenna is here!');
@@ -49,7 +126,7 @@ module.exports = (app, db) => {
 
             await conversation(id, data, type);
 
-            
+
             res.status(200).end();
         } else {
             console.log('WHY?');
