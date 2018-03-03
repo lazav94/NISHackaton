502569fdfed01a74b9afcf8a200392212a9801c0
lazar <lazar@spartans.tech>
2018-03-03 11:27:17 +0100

    :rocket:

--- conversation.js
+++ conversation.js
@@ -9,6 +9,28 @@ const {
     sendOffersList
 } = require('./messenger');
 
+const fuel = [{
+    name: 'G-Drive',
+    shortDescription: "Novi premijum benzin 100 oktana G-Drive 100 sadrži aktivne komponente koje omogućavaju efikasniji rad motora, poboljšavaju njegove performanse i pozitivno se odražavaju na njegovu ispravnost i dugovečnost. Proizvedeno prema najvišim tehnološkim standardima, G-Drive 100 je gorivo nove snage koje će vašem motoru pružiti profesionalnu zaštitu, a vama sigurniju i uzbudljiviju vožnju. Specijalna formula ovog goriva produžava radni vek motora i sprečava nastanak kvarova. ",
+    image_url: "https://www.nis.eu/wp-content/uploads/2014/10/gdrive.png"
+},
+{
+    name: 'EVRO BMB 98',
+    shortDescription: "Opis",
+    image_url: "https://www.nis.eu/wp-content/uploads/2014/10/nis_piu_logo_bmb98.png"
+},
+{
+    name: 'EVRO Dizel',
+    shortDescription: "Naš evrodizel vrhunskog kvaliteta omogućava maksimalne performanse vozila, pravilno paljenje i besprekoran rad motora čak i na izuzetno niskim temperaturama. Mirniji rad, veća snaga i manja potrošnja jesu prednosti korišćenja ovog goriva. Proizveden po najstrožim proizvođačkim specifikacijama, kvalitet evrodizela je potpuno u skladu sa evropskim standardom EN 590.",
+    image_url: "https://www.nis.eu/wp-content/uploads/2014/10/nis_piu_logo_dizel.png"
+},
+{
+    name: 'AUTOGAS TNG',
+    shortDescription: "TNG auto-gas prilagođen je uređajima za gas novije generacije. Ekonomičnost auto-gasa ogleda se u niskoj ceni, dužem veku trajanja motora, nižim troškovima održavanja vozila, dužem periodu zamene ulja i svećica, dužem veku trajanja katalizatora. Sa ekološkog aspekta, znatno je povoljniji od klasičnih motornih goriva. Proizveden po najstrožim proizvođačkim specifikacijama, kvalitet auto-gasa TNG potpuno je u skladu sa evropskim standardom EN 589.",
+    image_url: "https://www.nis.eu/wp-content/uploads/2014/10/nis_piu_logo_tng.png"
+}
+];
+
 let count = 0;
 
 module.exports = async (id, data, type) => {
@@ -25,27 +47,7 @@ module.exports = async (id, data, type) => {
                 let text = event.message.text
                 console.log('Text: ', text);
 
-                let fuel = [{
-                        name: 'G-Drive',
-                        shortDescription: "Novi premijum benzin 100 oktana G-Drive 100 sadrži aktivne komponente koje omogućavaju efikasniji rad motora, poboljšavaju njegove performanse i pozitivno se odražavaju na njegovu ispravnost i dugovečnost. Proizvedeno prema najvišim tehnološkim standardima, G-Drive 100 je gorivo nove snage koje će vašem motoru pružiti profesionalnu zaštitu, a vama sigurniju i uzbudljiviju vožnju. Specijalna formula ovog goriva produžava radni vek motora i sprečava nastanak kvarova. ",
-                        image_url: "https://www.nis.eu/wp-content/uploads/2014/10/gdrive.png"
-                    },
-                    {
-                        name: 'EVRO BMB 98',
-                        shortDescription: "Opis",
-                        image_url: "https://www.nis.eu/wp-content/uploads/2014/10/nis_piu_logo_bmb98.png"
-                    },
-                    {
-                        name: 'EVRO Dizel',
-                        shortDescription: "Naš evrodizel vrhunskog kvaliteta omogućava maksimalne performanse vozila, pravilno paljenje i besprekoran rad motora čak i na izuzetno niskim temperaturama. Mirniji rad, veća snaga i manja potrošnja jesu prednosti korišćenja ovog goriva. Proizveden po najstrožim proizvođačkim specifikacijama, kvalitet evrodizela je potpuno u skladu sa evropskim standardom EN 590.",
-                        image_url: "https://www.nis.eu/wp-content/uploads/2014/10/nis_piu_logo_dizel.png"
-                    },
-                    {
-                        name: 'AUTOGAS TNG',
-                        shortDescription: "TNG auto-gas prilagođen je uređajima za gas novije generacije. Ekonomičnost auto-gasa ogleda se u niskoj ceni, dužem veku trajanja motora, nižim troškovima održavanja vozila, dužem periodu zamene ulja i svećica, dužem veku trajanja katalizatora. Sa ekološkog aspekta, znatno je povoljniji od klasičnih motornih goriva. Proizveden po najstrožim proizvođačkim specifikacijama, kvalitet auto-gasa TNG potpuno je u skladu sa evropskim standardom EN 589.",
-                        image_url: "https://www.nis.eu/wp-content/uploads/2014/10/nis_piu_logo_tng.png"
-                    }
-                ];
+                
 
                 if (text === 'restart') {
                     count = 0;
